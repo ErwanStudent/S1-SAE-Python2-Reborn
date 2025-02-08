8dd5c05b5545fe497bb1eb90735cc72761407692
Erwan <24718500+ErwanGit@users.noreply.github.com>
2025-02-08 22:42:58 +0100

    chore: dualboot ubuntu in my pc, so no problems

--- source/start.sh
+++ source/start.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-#     # gnome-terminal -- bash -c "python3 serveur.py; exec bash"
-#     wsl.exe -e bash -c "python3 serveur.py; exec zsh" &
-# elif [[ "$OSTYPE" == "darwin"* ]]; then
-#     SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-#     osascript -e "tell application \"Terminal\" to do script \"cd $SCRIPT_DIR; python3 serveur.py\""
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    gnome-terminal -- bash -c "python3 serveur.py; exec bash"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    osascript -e "tell application \"Terminal\" to do script \"cd $SCRIPT_DIR; python3 serveur.py\""
 
-#     sleep 0.5
-# else
-#     echo "Unsupported OS"
-#     exit 1
-# fi
+    sleep 0.5
+else
+    echo "Unsupported OS"
+    exit 1
+fi
 
 python3 affichage.py&
 python3 IA.py --equipe joueur1&
